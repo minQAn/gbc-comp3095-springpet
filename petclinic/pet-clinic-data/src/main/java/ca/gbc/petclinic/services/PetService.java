ae02fadf589641e974455a1814fe8d158bc6e182
minQ <minq.an7@gmail.com>
2021-10-10 15:16:32 -0400

    Create Pet Clinic services. Closes #5

---
+++ petclinic/pet-clinic-data/src/main/java/ca/gbc/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package ca.gbc.petclinic.services;
+
+import ca.gbc.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+}
---
+++ petclinic/pet-clinic-data/src/main/java/ca/gbc/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package ca.gbc.petclinic.services;
+
+import ca.gbc.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
